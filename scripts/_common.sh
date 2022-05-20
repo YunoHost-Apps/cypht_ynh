299fb8b3b4f6aba29c0e4f5dc1e37c3814dd40af
ericgaspar <junk.eg@free.fr>
2022-05-20 23:06:58 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="8.0"
+
+# Composer version
+YNH_COMPOSER_VERSION="2.3.5"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-memcached php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-fileinfo"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
