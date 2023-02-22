935134c90bedfbbee30777cdf7532aa93cbe9c02
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-22 16:33:04 +0100

    Update updater.sh

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -14,8 +14,8 @@
 #=================================================
 
 # Fetching information
-current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
-repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
+current_version=$(cat manifest.toml | jq -j '.version|split("~")[0]')
+repo=$(cat manifest.toml | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
 version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
 assets="https://github.com/jasonmunro/cypht/archive/refs/tags/$version.tar.gz"
@@ -98,7 +98,7 @@ echo "... conf/$src.src updated"
 #=================================================
 
 # Replace new version in manifest
-echo "$(jq -s --indent 4 ".[] | .version = \"$version~ynh1\"" manifest.json)" > manifest.json
+echo "$(jq -s --indent 4 ".[] | .version = \"$version~ynh1\"" manifest.toml)" > manifest.toml
 
 # No need to update the README, yunohost-bot takes care of it
 
